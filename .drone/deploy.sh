b08f7eb6ac9c060ae9e861f9cbd8de44e3d92974
Kieran Coldron <kieran@coldron.com>
2018-03-25 15:54:36 +0100

    Remove compiled python code on git reset

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -1,8 +1,12 @@
 head=$(git rev-parse HEAD)
 hash=$(git log --pretty=%H --merges | sed -n 2p)
 git reset --hard $hash
+find . -name '*.pyc' | xargs -n 1 rm
+fab --list --short
 fab --list --short > ~/tasks_before
 git reset --hard $head
+find . -name '*.pyc' | xargs -n 1 rm
+fab --list --short
 fab --list --short > ~/tasks_after
 
 echo "New tasks to run:"
@@ -11,6 +15,9 @@ comm -13 --nocheck-order ~/tasks_before ~/tasks_after
 compare=$(comm -13 --nocheck-order ~/tasks_before ~/tasks_after)
 
 echo "$compare"
+
+IFS='
+'
 
 for x in $compare; do
    fab install -R desktops "$x"
