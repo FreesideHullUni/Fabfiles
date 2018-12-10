7dceed0050d7a56b9526c716c637f5b89acf64d0
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2018-12-10 10:19:49 +0000

    Use find -delete instead of xargs

--- common/deploy.sh
+++ common/deploy.sh
@@ -1,11 +1,11 @@
 head=$(git rev-parse HEAD)
 hash=$(git log --pretty=%H --merges | sed -n 3p)
 git reset --hard $hash
-find . -name '*.pyc' | xargs -n 1 rm
+find . -name '*.pyc' -delete
 fab --list --short
 fab --list --short > ~/tasks_before
 git reset --hard $head
-find . -name '*.pyc' | xargs -n 1 rm
+find . -name '*.pyc' -delete
 fab --list --short
 fab --list --short > ~/tasks_after
 
