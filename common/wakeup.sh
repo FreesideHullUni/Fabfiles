80c0a1197e2de797122aeff5dbe69c95d7024b86
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2018-12-10 10:22:14 +0000

    Update MAC addresses in wakeup.sh

--- common/wakeup.sh
+++ common/wakeup.sh
@@ -1,7 +1,7 @@
 echo  "Send WOL packets"
-ether-wake -i eno1 '07c:05:7:0c:c5:e5'
+ether-wake -i eno1 '7c:05:07:0c:c5:e5'
 ether-wake -i eno1 '7c:05:07:10:6b:37'
-ether-wake -i eno1 '52:54:00:77:9c:1e'
+ether-wake -i eno1 '7c:05:07:0d:5b:fb'
 echo  "Waiting for desktops to start"
 until nc -vzw 2 fs-desktop-01.freeside.co.uk 22; do sleep 2; done
 until nc -vzw 2 fs-desktop-02.freeside.co.uk 22; do sleep 2; done
